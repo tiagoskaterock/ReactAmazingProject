9ee7647cbe3ab935ea44537573086e401b537692
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-07-14 14:34:54 -0300

    Handle button click

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,9 +1,14 @@
 import '../css/cta.css'
 
 function Cta() {
+
+    function handleClick() {
+        alert('Congratulations! You are now subscribed to our Newsletter!!!')
+    }
+
     return (
         <section className="text-center section-cta">
-            <button className="btn btn-primary">
+            <button className="btn btn-primary" onClick={handleClick}>
                 <i className="fas fa-rss mr-1"></i>
                 Subscribe me!
             </button>
