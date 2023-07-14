b138f96b34817f3d35f93926778aa1f8c56c288c
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-07-14 07:07:08 -0300

    Using BootStrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Profile from "./components/Profile";
+import Title from "./components/Title";
 
 function App() {
   return (
-    <div>
-      <h1>Amazing React Project!</h1>
+    <div className="row">
+      <h1 className="text-center"><Title /></h1>
       <Profile img="img/profiles/1.jpg" title="Amanda"/>
       <Profile img="img/profiles/2.jpg" title="Larissa"/>
       <Profile img="img/profiles/3.jpg" title="Sara"/>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,10 @@
+import '../App.css'
+
 function Profile(props) {
     return (
-        <div>
+        <div className='profile col-md-4'>
             <img src={props.img} alt={props.title} title={props.title}/>
+            <h2 className='text-center'>{props.title}</h2>
         </div>
     )
 }
