ab553ff1366993dfe7cdd940b91aed6379eb7cb5
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-07-14 13:44:08 -0300

    CAT Section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Team from "./components/Team"
 import Title from "./components/Title"
 import About from "./components/About"
 import Footer from "./components/Footer";
+import Cta from "./components/Cta";
 
 function App() {
   return (
     <div>
       <h1 className="text-center py-5"><Title /></h1>
       <About />
-      <Team />
+      <Cta />
+      <Team />      
       <Footer />
     </div>
   );
---
+++ src/components/Cta.jsx
@@ -0,0 +1,14 @@
+import '../css/cta.css'
+
+function Cta() {
+    return (
+        <section className="text-center section-cta">
+            <button className="btn btn-primary">
+                <i className="fas fa-rss mr-1"></i>
+                Subscribe me!
+            </button>
+        </section>        
+    )
+}
+
+export default Cta
